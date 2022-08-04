7a1cb90acb7d46fabe890f219270459f28f9b2c0
Joao Romeiro <j.romeiro@live.com>
2022-08-04 16:04:31 -0300

    Segundo Commit

--- src/routes.php
+++ src/routes.php
@@ -6,5 +6,6 @@ Route::get('calculator', function () {
     echo "Hello from calculator package";
 });
 
+//
 Route::get('add/{a}/{b}', [\jromeiro\Calculator\CalculatorController::class, 'add']);
 Route::get('substract/{a}/{b}', [\jromeiro\Calculator\CalculatorController::class, 'substract']);
